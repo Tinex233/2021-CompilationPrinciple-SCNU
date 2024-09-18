aff400c4e3f8fabfe272c3e0629307d0cca2d374
Tinex233 <128806135+Tinex233@users.noreply.github.com>
2024-09-18 21:25:10 +0800

    Update main.cpp

--- 源程序/pro1/CompilationPrinciplePro1/main.cpp
+++ 源程序/pro1/CompilationPrinciplePro1/main.cpp
@@ -2,7 +2,7 @@
  * Copyright 2024 Li WenJie. All Rights Reserved.
  * File: main.cpp
  * Version: 1.0
- * Author: Li WenJie
+ * Author: Tinex
  * Date: May 15, 2024
  * Description: Contains the main function of the QT program, where a MainWindow window object is created.
 */
